9fabf3ccecc74a5cb2f99d0d4766c80b8769f346
Louis <louis.vanoise@epitech.eu>
2023-07-03 15:52:42 +0200

    fix link side bar

--- src/ui/components/SideBar/Link/index.tsx
+++ src/ui/components/SideBar/Link/index.tsx
@@ -1,20 +1,30 @@
 import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link } from "react-router-dom";
+import { IconDefinition } from "@fortawesome/free-solid-svg-icons";
 
 interface SideBarLinkProps {
   text: string;
   onClick?: () => void;
+  link?: string;
+  logo: IconDefinition;
 }
 
-const SideBarLink = ({ text, onClick }: SideBarLinkProps) => {
+const SideBarLink = ({ text, onClick, link, logo }: SideBarLinkProps) => {
   return (
-    <li
-      onClick={onClick}
-      className="rounded-full px-4 py-2 text-sm justify-between flex items-center text-black ease-out duration-300  cursor-pointer hover:text-gray-900"
-    >
-      <span className="self-center">{text}</span>
-      <FontAwesomeIcon icon={faChevronDown} />
-    </li>
+    <Link to={link ? link : "/user"}>
+      <li
+        onClick={onClick}
+        className="rounded-full px-4 py-2 text-sm justify-between flex items-center text-black ease-out duration-300  cursor-pointer hover:text-gray-900"
+      >
+        {" "}
+        <span className="self-center flex gap-2 items-center">
+          <FontAwesomeIcon icon={logo} />
+          {text}
+        </span>
+        <FontAwesomeIcon icon={faChevronDown} />
+      </li>
+    </Link>
   );
 };
 
--- src/ui/components/SideBar/index.tsx
+++ src/ui/components/SideBar/index.tsx
@@ -1,23 +1,35 @@
 import { Link } from "react-router-dom";
 import logo from "./../../../assets/Global/La_Note.png";
 import SideBarLink from "./Link";
+import Input from "../Input";
+import { faHouse } from "@fortawesome/free-solid-svg-icons";
 
 const SideBar = () => {
   const links = [
     {
       label: "Dashboard",
+      link: "/",
+      logo: faHouse,
     },
     {
       label: "Client",
+      link: "/user",
+      logo: faHouse,
     },
     {
       label: "Creation",
+      link: "/user",
+      logo: faHouse,
     },
     {
       label: "Resume",
+      link: "/user",
+      logo: faHouse,
     },
     {
       label: "Setting",
+      link: "/user",
+      logo: faHouse,
     },
   ];
 
@@ -29,9 +41,12 @@ const SideBar = () => {
           La Note
         </h1>
       </Link>
-      <ul className="flex flex-col  gap-3 self-center w-full">
+      <Input type="text" placeholder="Search..." name="search" />
+      <ul className="flex flex-col  gap-3 self-center w-full mt-5">
         {links.map((link) => {
-          return <SideBarLink text={link.label} />;
+          return (
+            <SideBarLink text={link.label} link={link.link} logo={link.logo} />
+          );
         })}
       </ul>
     </div>
